c49919773ac8e82b955752ae009fb6cbe92e8cdf
Andrius Martinkėnas <andrius.martinkenas321@gmail.com>
2022-07-11 14:54:41 +0300

    header scroll

--- js/components/myHeader.js
+++ js/components/myHeader.js
@@ -1,22 +1,24 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if ((document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) && window.innerWidth > 786) {
+  if ((document.body.scrollTop > 0 || document.documentElement.scrollTop > 350) && window.innerWidth > 0) {
     document.getElementById("header").className = "container head head-sticky";
-    document.getElementById("headcol").className = "col-12 navmenu";
-    document.getElementById("logo").src = "./img/header-logo-blue.png";
+    document.getElementById("headcol").className = " navmenu";
+    document.getElementById("logo").src = "./img/logo.png";
         let nodes = document.querySelectorAll(".mainlinks");
         for (let i = 0; i < nodes.length; i++) {
-        nodes[i].style.color = "#000";
+        nodes[i].style.color = "var(--secondary-nav-color)";
+        nodes[0].style.color = "black";
         
       }
-  } else {
+  } else if(window.innerWidth > 0){
     document.getElementById("header").className = "container head head-fixed";
-    document.getElementById("headcol").className = "col-12 col-lg-10 navmenu";
-    document.getElementById("logo").src = "./img/header-logo-white.png";
+    document.getElementById("headcol").className = " navmenu";
+    document.getElementById("logo").src = "./img/logo-2.png";
         let nodes = document.querySelectorAll(".mainlinks");
         for (let i = 0; i < nodes.length; i++) {
-        nodes[i].style.color = "#fff";
+        nodes[i].style.color = "white";
+        nodes[0].style.color = "var(--secondary-nav-color)";
   }
 } 
 }
